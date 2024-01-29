a38e0cf02ee1f39b2d66c386a668b714e46959ed
Danniel Navas <dannielnavas@gmail.com>
2024-01-28 21:58:20 -0500

    ✨ Add routes for landing and login pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./domains/landing/pages/landing/landing.component').then(
+        (m) => m.LandingComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./domains/side/pages/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+];
