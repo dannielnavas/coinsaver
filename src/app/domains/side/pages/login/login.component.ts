9371b388b4e751e149e2a1840d5911a7190dacf5
Danniel Navas <dannielnavas@gmail.com>
2024-01-28 21:58:11 -0500

    ✨ Add login buttons and functionality

--- src/app/domains/side/pages/login/login.component.ts
+++ src/app/domains/side/pages/login/login.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import {
+  Auth,
+  GoogleAuthProvider,
+  signInAnonymously,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  redirect = ['/'];
 
+  constructor(@Optional() private auth: Auth, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  async loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const data = await signInWithPopup(this.auth, provider);
+    console.log(data);
+    await this.router.navigate(this.redirect);
+  }
+
+  async loginAnonymously() {
+    await signInAnonymously(this.auth);
+    await this.router.navigate(this.redirect);
+  }
+
+  async loginWithEmailAndPassword() {
+    // const data = await createUserWithEmailAndPassword(
+    //   this.auth,
+    //   'darker13@outlook.com',
+    //   '1602003139Odin!'
+    // );
+    // console.log(data);
+    const user = await signInWithEmailAndPassword(
+      this.auth,
+      'darker13@outlook.com',
+      '1602003139Odin!'
+    );
+    console.log(user);
+    await this.router.navigate(this.redirect);
+  }
 }
