324581ce721ffdac62a4d18026540ab7f367ae34
Danniel Navas <dannielnavas@gmail.com>
2024-01-28 21:58:45 -0500

    ✨ Update landing page with new content and login link

--- src/app/domains/landing/pages/landing/landing.component.ts
+++ src/app/domains/landing/pages/landing/landing.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkWithHref],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.component.scss',
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}
