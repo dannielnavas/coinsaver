498b9272dc37b5d145f5c63d3750b75f0f387d6f
Danniel Navas <dannielnavas@gmail.com>
2024-03-05 22:15:24 -0500

    ✨ Corregir enlace de imagen de avatar en el componente de panel de control

--- src/app/domains/private-side/pages/dashboard/dashboard.component.ts
+++ src/app/domains/private-side/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,7 @@
 import { NavbarComponent } from '@/app/shared/components/navbar/navbar.component';
-import { IUserGoogle } from '@/app/shared/models/user-google.model';
 import { SessionService } from '@/app/shared/services/session.service';
 import { Component, inject, signal } from '@angular/core';
-import {
-  Firestore,
-  addDoc,
-  collection,
-  getDocs,
-  query,
-} from '@angular/fire/firestore';
+import { UserCredential } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,44 +11,15 @@ import {
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-  firestore = inject(Firestore);
   private sessionService = inject(SessionService);
-  user = signal<IUserGoogle>({} as IUserGoogle);
+  user = signal<UserCredential | null>(null);
 
   ngOnInit() {
     this.getDataUser();
-    this.addDoc();
-    this.filterIncomeUser();
   }
 
-  getDataUser() {
-    const user = this.sessionService.getUser();
-    this.user.set(user);
-  }
-
-  async getData() {
-    const data = (
-      await getDocs(query(collection(this.firestore, 'income')))
-    ).docs.map((data) => data.data());
-    console.log(data);
-  }
-
-  async filterIncomeUser() {
-    await getDocs(collection(this.firestore, 'income')).then(
-      (querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          console.log(doc.id, ' => ', doc.data());
-        });
-      }
-    );
-  }
-
-  async addDoc() {
-    const user = this.sessionService.getUser();
-    const docRef = await addDoc(collection(this.firestore, 'income'), {
-      name: 'Salario',
-      amount: 1000,
-      user: this.user().user.uid,
-    });
+  async getDataUser() {
+    const user = await this.sessionService.getUser();
+    this.user.set(user.value); // Extract the value from the BehaviorSubject
   }
 }
