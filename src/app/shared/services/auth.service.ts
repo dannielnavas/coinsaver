087567025f78cf378ab90345d6c9cf65c12fb025
Danniel Navas <dannielnavas@gmail.com>
2024-03-05 22:15:41 -0500

    ✨ Agregadas funciones de inicio de sesión con Google, anónimo y con correo electrónico en AuthService

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional, inject } from '@angular/core';
+import {
+  Auth,
+  GoogleAuthProvider,
+  UserCredential,
+  signInAnonymously,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  @Optional() private auth = inject(Auth);
+
+  async googleLogin(): Promise<UserCredential> {
+    const provider = new GoogleAuthProvider();
+    return await signInWithPopup(this.auth, provider);
+  }
+
+  async anomymousLogin() {
+    return await signInAnonymously(this.auth);
+  }
+
+  async loginWithEmailAndPassword(email: string, password: string) {
+    return await signInWithEmailAndPassword(this.auth, email, password);
+  }
 }
